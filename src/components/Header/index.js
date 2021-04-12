d9bdffe51696b10a9714aefa37fa141b1b53d296
Anjali Sheth <anjali.sheth@otis.com>
2021-04-12 19:16:50 -0400

    gh deployment

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h2>Employee Directory</h2>
+            <h4> Click on Column Headings to Sort </h4>
+           
+        </div>
+    )
+}
+
+export default Header;
+
+
